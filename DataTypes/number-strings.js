4f7d5a0cadd5020f9a7f9ba8789af3cd4fd5270e
Suddhaswatta <suddhaswatta.maity1@gmail.com>
2021-11-07 18:01:38 +0530

    number

---
+++ DataTypes/number-strings.js
@@ -0,0 +1,14 @@
+const n = 10
+const circle = (r)=>{
+    return Math.PI*Math.pow(r,2)
+}
+console.log(`Area of circle ${circle(3)} `);
+console.log(typeof n);
+
+console.log(Math.PI);
+console.log(Math.random());
+
+/**
+ * Strings
+ */
+
